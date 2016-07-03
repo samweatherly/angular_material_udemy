2dccd6b8fefab992a0b124fd7c2e4db163401f1a
Sam Weatherly <weatherlysam@gmail.com>
2016-07-03 15:19:25 -0700

    display problem - carrd only containts image

--- scripts/app.js
+++ scripts/app.js
@@ -4,10 +4,5 @@ angular
         $mdThemingProvider.theme('default')
             .primaryPalette('teal')
             .accentPalette('orange');
-    })
-    .directive("helloWorld", function() {
-        return {
-            template: "<h1>{{ message }}</h1>"
-        }
     });
     
